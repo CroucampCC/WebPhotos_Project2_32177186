9711e9175c6c11569c46b7c91821a82c955420fc
Christo <jpcroucamp@gmail.com>
2021-10-31 12:47:35 +0200

    Start of the Project

---
+++ src/test/java/com/CC/Webphoto/source/WebphotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CC.Webphoto.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebphotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
