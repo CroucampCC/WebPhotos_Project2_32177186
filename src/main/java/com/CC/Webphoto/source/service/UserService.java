753c37d293f55290ff426bf02d3aa0c699e9361b
Christo <jpcroucamp@gmail.com>
2021-11-04 21:56:25 +0200

    User Services implemented Save,Delete, Find, All

---
+++ src/main/java/com/CC/Webphoto/source/service/UserService.java
@@ -0,0 +1,17 @@
+package com.CC.Webphoto.source.service;
+
+import com.CC.Webphoto.source.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+
+    User findByUsername(String username);
+
+    List<User> findAllUsers();
+}
---
+++ src/main/java/com/CC/Webphoto/source/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.CC.Webphoto.source.service;
+
+import com.CC.Webphoto.source.model.User;
+import com.CC.Webphoto.source.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //Security
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return  userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long userId){
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Long numberOfUsers(){
+        return userRepository.count();
+    }
+
+}
